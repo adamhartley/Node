6f283d6777a5963205bcbe9949987e3605345d20
Adam Hartley <ahartley@benevity.com>
2020-05-05 08:50:32 -0600

    Adding redirection with form, file containing user input is written, and user is redirected back to the home page.

--- basics/app.js
+++ basics/app.js
@@ -1,8 +1,43 @@
+const fs = require('fs');
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    const method = req.method;
+    /*
+     * On the homepage, the user will see a form to enter data, and an input button.
+     * The input data will be saved in a file on the server.
+     */
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
+        res.write('</html>');
+        return res.end(); // exit function
+    }
 
+    /*
+     * Redirect the user back to the home page after the form is submitted and store the file
+     */
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        // adding listener for data event, which is fired whenever a new chunk is ready to be read
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        // register end listener, which is executed when the incoming request data has finished being parsed
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        })
+
+        res.statusCode = 302;// 302 - redirect status code
+        res.setHeader('Location', '/');
+        return res.end();
+    }
     // set response header
     res.setHeader('Content-Type', 'text/html');
     // write data to the response
