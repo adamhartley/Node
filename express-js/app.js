bde0731084062703af142d39e962ce8a7c7a58f1
Adam Hartley <ahartley@benevity.com>
2020-05-07 18:00:48 -0600

    express-js: Adding basic middleware functions to filter incoming requests

--- express-js/app.js
+++ express-js/app.js
@@ -1,27 +1,29 @@
-const http = require('http');
 const express = require('express');
 
 // create an express app
 const app = express();
 
 /*
- * use() allows us to add other middleware frameworks to express
- * The function passed to use() will be executed for every incoming request
+ * use() mounts the specified middleware function or functions at the specified path: the middleware
+ * function is executed when the base of the requested path matches path.
  *
  * req - request
  * res - response
  * next - function that is passed along to the next middleware in line
  */
-app.use((req, res, next) => {
-    console.log('In the middleware!!!');
-    next(); // allows the request to continue to the next middleware in line
+app.use('/', (req, res, next) => {
+    console.log('This always runs');
+    next();
 });
 
-app.use((req, res, next) => {
-    console.log('In another middleware!!!');
+app.use('/add-product', (req, res, next) => {
+    console.log('In add-product middleware!!!');
+    res.send('<h1>Add product page!!</h1>');
+});
+
+app.use('/', (req, res, next) => {
+    console.log('In home page middleware!!!');
     res.send('<h1>Hello from Express!</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
