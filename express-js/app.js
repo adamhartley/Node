bf18d75e6bd5b7b807c43ff9d600632412dd1eae
Adam Hartley <ahartley@benevity.com>
2020-05-08 11:33:04 -0600

    express-js: Adding html files for serving pages, and basic CSS styling

--- express-js/app.js
+++ express-js/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 // create an express app
 const app = express();
@@ -7,32 +11,12 @@ const app = express();
 // add request parser (body-parser package)
 app.use(bodyParser.urlencoded({extended: false}));
 
-/*
- * use() mounts the specified middleware function or functions at the specified path: the middleware
- * function is executed when the base of the requested path matches path.
- *
- * req - request
- * res - response
- * next - function that is passed along to the next middleware in line
- */
-app.use('/', (req, res, next) => {
-    console.log('This always runs');
-    next();
-});
+app.use('/admin', adminRoutes); // register admin routes
+app.use(shopRoutes); // register shop routes
 
-app.use('/add-product', (req, res, next) => {
-    console.log('In add-product middleware!!!');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Producct</button></form>');
-});
-
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
+// catch all route: if we made it through all the routes, return a 404 page not found
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
-
-app.use('/', (req, res, next) => {
-    console.log('In home page middleware!!!');
-    res.send('<h1>Hello from Express!</h1>');
-});
 
 app.listen(3000);
