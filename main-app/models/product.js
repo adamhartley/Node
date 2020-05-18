2d4e4a40f091d36fbcbf1fd5528696fa51900764
ahartley <adam.wd.hartley@gmail.com>
2020-05-17 22:21:03 -0600

    Updated models to read and write product data to file.

--- main-app/models/product.js
+++ main-app/models/product.js
@@ -1,7 +1,25 @@
 /*
  * The core Product entity
  */
-const products = [];
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../util/path');
+const p = path.join(
+    rootDir,
+    'data',
+    'products.json'
+);
+
+// Helper function to read products from the file
+const getProductsFromFile = callback => {
+    // Read the products.json file into memory. There are more efficient ways (e.g. createReadStream), but this is a small dataset.
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            return callback([]); // no products found, return empty array
+        }
+        callback(JSON.parse(fileContent));
+    });
+}
 
 module.exports = class Product {
     constructor(title) {
@@ -9,10 +27,16 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this)
+        getProductsFromFile(products => {
+            // append product to array of products
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log('Error writing products file ' + err);
+            });
+        })
     }
 
-    static fetchAll() {
-        return products;
+    static fetchAll(callback) {
+        getProductsFromFile(callback);
     }
-}
+};
